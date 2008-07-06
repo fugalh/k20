2159f0badbe12d3260aa22fd079524f2a9cd0397
Hans Fugal <hans@fugal.net>
2008-07-05 22:17:40 -0600

    simple bars

--- k20.c
+++ k20.c
@@ -1,2 +1,22 @@
-#include <curses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <jack/jack.h>
+
+static void finish(int sig);
+int *jack_process(jack_nframes_t, void*);
+
+int main(int argc, char **argv)
+{
+    signal(SIGINT, finish);
+
+    printf("-50        40        30        20   15   10  6  3  0  3  6   10   15   20+\n");
+    printf(" |         |         |         |    |    |   |  |  |  |  |   |    |    |\n");
+    
+    finish(0);
+}
+
+static void finish(int sig)
+{
+    exit(0);
+}
 
